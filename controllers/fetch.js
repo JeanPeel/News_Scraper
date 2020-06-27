56cb35092860b0d540c8c07c41f6ac7a54bab19c
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-26 20:31:45 -0600

    added fetch.js

---
+++ controllers/fetch.js
@@ -0,0 +1,33 @@
+// Controller of our scraper
+
+var db = require('../models');
+var scrape = require('../scriptw/scrape');
+
+module.exports{
+    scrapeHeadlines: function(req, res) {
+        // scrape the NYT
+        return scrape()
+            .then(function (articles) {
+                return db.Headline.create(articles);
+            })
+            .then(function (dbHeadline) {
+                if (dbHeadline.length === 0) {
+                    res.json({
+                        message: 'no new articles today.  Check back tomorrow!'
+                    });
+                }
+                else {
+                    // otherwise send back a count of how many articles we have
+                    res.json({
+                        message: 'Added ' + dbHeadline.length + ' new articles!'
+                    }):
+                }
+            })
+            .catch(function (err) {
+                // this query wont insert articles with duplicate headlines,
+                res.json({
+                    message: 'Scrape complete!!'
+                });
+            });
+    }
+}
