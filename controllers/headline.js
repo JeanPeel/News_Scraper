33e5f97e14d32a9b2fe30fb47f69e18afc22a110
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-30 20:27:27 -0600

    edited errors on controllers folder

--- controllers/headline.js
+++ controllers/headline.js
@@ -1,26 +1,28 @@
-// Controller for our Headlines
-var db = require('../models');
+// Controller for our headlines
+// ============================
+var db = require("../models");
 
 module.exports = {
-    findAll: function (req, res) {
-        db.Headline
-            .find(req.query)
-            .sort({ date: -1 })
-            .then(function (dbHeadline) {
-                res.json(dbHeadline);
-            });
-    },
-    // Delete the specified headline
-    delete: function (req, res) {
-        db.Headline.remove({ _id: req.parms.id }).then(function (dbHeadline) {
-            res.json(dbHeadline);
-        });
-    },
-    //  Update the specified headline
-    update: function (req, res) {
-        db.Headline.findOneandUpdate({ _id: req.parms.id }, { $set: req.body },
-            { new: ture }).then(function (dbheadline) {
-                res.json(dbHeadline);
-            });
-    }
+  // Find all headlines, sort them by date, send them back to the user
+  findAll: function(req, res) {
+    db.Headline
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(function(dbHeadline) {
+        res.json(dbHeadline);
+      });
+  },
+  // Delete the specified headline
+  delete: function(req, res) {
+    db.Headline.remove({ _id: req.params.id }).then(function(dbHeadline) {
+      res.json(dbHeadline);
+    });
+  },
+  // Update the specified headline
+  update: function(req, res) {
+    db.Headline.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, 
+      { new: true }).then(function(dbHeadline) {
+      res.json(dbHeadline);
+    });
+  }
 };
